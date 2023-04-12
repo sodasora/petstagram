49999faa99292008249987b8747711d78dc14eb7
Lee Hyeonsik <hyeon5819@gmail.com>
2023-04-12 23:20:07 +0900

    feed_second

--- petstagram/urls.py
+++ petstagram/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
     path('', include('feed.urls')),
-    # path('', include('board.urls')),
+    path('', include('board.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
