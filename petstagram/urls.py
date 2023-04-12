f8acfbfbfaa1ad522c2698567e400c5536674bd4
Lee Hyeonsik <hyeon5819@gmail.com>
2023-04-12 22:42:42 +0900

    feed_commit

--- petstagram/urls.py
+++ petstagram/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
+    path('', include('feed.urls')),
+    # path('', include('board.urls')),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
