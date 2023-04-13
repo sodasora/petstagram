dc67d29e29a03681b6601f3085d83ca5282c2ca5
Woo Sora <awo3sr@gmail.com>
2023-04-13 20:35:32 +0900

    detailpost added

---
+++ detailpost/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from user.models import UserModel
+
+
+# Create your models here.
+class DetailPost(models.Model):  # 새로운 모델을 생성
+    class Meta:
+        db_table = "detail"
+
+    title = models.CharField("제목",max_length=50)
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    content = models.CharField("내용",max_length=225)
+    create_at = models.DateTimeField("작성시간",auto_now_add=True)
+    update_at = models.DateTimeField("수정시간",auto_now=True)
---
+++ detailpost/urls.py
@@ -0,0 +1,10 @@
+# tweet/urls.py
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('detailpost/<int:id>', views.detail_post, name='detail_post'),
+    path('detailpost/delete/<int:id>', views.delete_post, name='delete_post'),
+]
