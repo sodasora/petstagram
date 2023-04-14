9913fb39dc8e5e2a8a59687481bdb365072b39dd
Lee Hyeonsik <hyeon5819@gmail.com>
2023-04-14 21:11:12 +0900

    feed app delete, related file edit

--- petstagram/urls.py
+++ petstagram/urls.py
@@ -22,7 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
-    path('', include('feed.urls')),
     path('', include('board.urls')),
     path('', include('detailpost.urls')),
 ]
